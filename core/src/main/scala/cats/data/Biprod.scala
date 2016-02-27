5061f9b4530298401d5cb458ca3713798e5042b2
Adelbert Chang <adelbertc@gmail.com>
2016-02-26 21:49:38 -0800

    More Biprod

--- core/src/main/scala/cats/data/Biprod.scala
+++ core/src/main/scala/cats/data/Biprod.scala
@@ -7,7 +7,21 @@ final case class Biprod[F[_, _], G[_, _], A, B](first: F[A, B], second: G[A, B])
 
 object Biprod extends BiprodInstances
 
-private[data] sealed abstract class BiprodInstances {
+private[data] sealed abstract class BiprodInstances extends BiprodInstances1 {
+  implicit def biprodEq[F[_, _], G[_, _], A, B](implicit FAB: Eq[F[A, B]], GAB: Eq[G[A, B]]): Eq[Biprod[F, G, A, B]] =
+    new Eq[Biprod[F, G, A, B]] {
+      def eqv(x: Biprod[F, G, A, B], y: Biprod[F, G, A, B]): Boolean =
+        FAB.eqv(x.first, y.first) && GAB.eqv(x.second, y.second)
+    }
+
+  implicit def biprodBitraverse[F[_, _]: Bitraverse, G[_, _]: Bitraverse]: Bitraverse[Biprod[F, G, ?, ?]] =
+    new BiprodBitraverse[F, G] {
+      val F = Bitraverse[F]
+      val G = Bitraverse[G]
+    }
+}
+
+private[data] sealed abstract class BiprodInstances1 {
   implicit def biprodBifunctor[F[_, _]: Bifunctor, G[_, _]: Bifunctor]: Bifunctor[Biprod[F, G, ?, ?]] =
     new BiprodBifunctor[F, G] {
       val F = Bifunctor[F]
@@ -21,7 +35,7 @@ private[data] sealed abstract class BiprodInstances {
     }
 }
 
-private[data] sealed abstract class BiprodBifunctor[F[_, _], G[_, _]] extends Bifunctor[Biprod[F, G, ?, ?]] {
+private[data] sealed trait BiprodBifunctor[F[_, _], G[_, _]] extends Bifunctor[Biprod[F, G, ?, ?]] {
   def F: Bifunctor[F]
   def G: Bifunctor[G]
 
@@ -29,7 +43,7 @@ private[data] sealed abstract class BiprodBifunctor[F[_, _], G[_, _]] extends Bi
     Biprod(F.bimap(fab.first)(f, g), G.bimap(fab.second)(f, g))
 }
 
-private[data] sealed abstract class BiprodBifoldable[F[_, _], G[_, _]] extends Bifoldable[Biprod[F, G, ?, ?]] {
+private[data] sealed trait BiprodBifoldable[F[_, _], G[_, _]] extends Bifoldable[Biprod[F, G, ?, ?]] {
   def F: Bifoldable[F]
   def G: Bifoldable[G]
 
@@ -42,4 +56,21 @@ private[data] sealed abstract class BiprodBifoldable[F[_, _], G[_, _]] extends B
     val first = F.bifoldRight(fab.first, c)(f, g)
     G.bifoldRight(fab.second, first)(f, g)
   }
+}
+
+private[data] sealed abstract class BiprodBitraverse[F[_, _], G[_, _]]
+    extends Bitraverse[Biprod[F, G, ?, ?]]
+    with BiprodBifoldable[F, G]
+    with BiprodBifunctor[F, G] {
+  def F: Bitraverse[F]
+  def G: Bitraverse[G]
+
+  def bitraverse[H[_], A, B, C, D](fab: Biprod[F, G, A, B])(f: A => H[C], g: B => H[D])(implicit H: Applicative[H]): H[Biprod[F, G, C, D]] = {
+    val first = F.bitraverse(fab.first)(f, g)
+    val second = G.bitraverse(fab.second)(f, g)
+    H.map2(first, second) { case (fcd, gcd) => Biprod(fcd, gcd) }
+  }
+
+  override def bimap[A, B, C, D](fab: Biprod[F, G, A, B])(f: A => C, g: B => D): Biprod[F, G, C, D] =
+    Biprod(F.bimap(fab.first)(f, g), G.bimap(fab.second)(f, g))
 }
--- tests/src/test/scala/cats/tests/BiprodTests.scala
+++ tests/src/test/scala/cats/tests/BiprodTests.scala
@@ -3,7 +3,10 @@ package tests
 
 import cats.data.{Biprod, Xor}
 import cats.laws.discipline._
+import cats.laws.discipline.arbitrary._
+import cats.laws.discipline.eq.tuple2Eq
 
 class BiprodTests extends CatsSuite {
-  checkAll("Biprod[Xor, Tuple2, ?, ?]", BifunctorTests[Biprod[Xor, Xor, ?, ?]])
+  checkAll("Biprod[Xor, Tuple2, ?, ?]", BitraverseTests[Biprod[Xor, Tuple2, ?, ?]].bitraverse[Option, Int, Int, Int, String, String, String])
+  checkAll("Bitraverse[Biprod[Xor, Tuple2, ?, ?]]", SerializableTests.serializable(Bitraverse[Biprod[Xor, Tuple2, ?, ?]]))
 }
